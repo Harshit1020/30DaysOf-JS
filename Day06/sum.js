fab813bf212c79a157e5dda81fc642a0bde2399d
Harshit_Dongre <95414266+Harshit1020@users.noreply.github.com>
2023-11-11 22:14:52 +0530

    day06

---
+++ Day06/sum.js
@@ -0,0 +1,13 @@
+// **Challenge 6**  
+// Write a JavaScript program to compute the sum of 
+// the two given integers. If the two values are 
+//  same, then return triple their sum.
+
+
+function sum(a,b){
+    if(a == b) return 3*(a+b)
+    return a+b;
+}
+let a = 7;
+let b = 7
+console.log(sum(7,7))
