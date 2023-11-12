da74c036daa90586dfb43ccd5ae165c1d8f81c6a
Harshit_Dongre <95414266+Harshit1020@users.noreply.github.com>
2023-11-12 23:33:53 +0530

    day07

--- Day06/sum.js
+++ Day06/sum.js
@@ -2,8 +2,6 @@
 // Write a JavaScript program to compute the sum of 
 // the two given integers. If the two values are 
 //  same, then return triple their sum.
-
-
 function sum(a,b){
     if(a == b) return 3*(a+b)
     return a+b;
---
+++ Day07/pair.js
@@ -0,0 +1,12 @@
+// **Challenge 7**  
+// Write a JavaScript program to check a pair of 
+// numbers and return true if one of the 
+// numbers is 50 or if their sum is 50.
+
+
+function checkPair(a,b){
+    return (a == 50 || b  == 50 || a+b == 50)
+}
+
+console.log(checkPair(44,6))
+
